52284d615be0c2c8681f113399f99a3db01fc06e
Gilles <gilles.h@iabsis.com>
2021-09-22 14:58:01 +0200

    Added comments

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 const EC = require('elliptic').ec;
 const { program } = require('commander');
+
+// Initialize the command line arguments
+// Use node index --help to display the arguments details
 program
     .version('0.0.1')
     .description("This command verify the DER elliptic signature")
@@ -9,18 +12,23 @@ program
 
 program.parse();
 
+
+// Get required parameters coming from command line
 const publicKey = program.args[0];
 const messageHash = program.args[1];
 const signature = program.args[2];
 
+
+// Initialize elliptic with the secp256k1 algorithm
 var ec = new EC('secp256k1');
 
-verifySignature = (publicKey, signature, messageHash) => {
+// Function to check the validity of the signature
+const verifySignature = (publicKey, signature, messageHash) => {
     let key = ec.keyFromPublic(publicKey, 'hex');
     return key.verify(messageHash, signature);
 }
 
-
+// Run the signature verification
 try {
     if (verifySignature(publicKey, signature, messageHash)) {
         console.log("Signature is correct");
